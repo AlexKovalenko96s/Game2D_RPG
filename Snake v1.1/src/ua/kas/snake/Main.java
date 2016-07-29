168e69686067d600ed50ee7105eebb11f3f7f810
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-07-30 01:02:45 +0300

    so complete

--- Snake v1.1/src/ua/kas/snake/Main.java
+++ Snake v1.1/src/ua/kas/snake/Main.java
@@ -7,16 +7,21 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.io.File;
 import java.util.ArrayList;
-import java.util.Random;
+import java.util.Scanner;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
 public class Main implements ActionListener, KeyListener {
 
-	public ArrayList<Point> snakeParts = new ArrayList<Point>();
+	public static ArrayList<Point> snakeParts = new ArrayList<Point>();
 	public ArrayList<String> grassPoints = new ArrayList<String>();
+
+	private Scanner scn;
+
+	private String Map[] = new String[25];
 
 	public static final int UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3;
 
@@ -26,22 +31,22 @@ public class Main implements ActionListener, KeyListener {
 	public static final int WIDTH = 966;
 	public static final int SCALE = 32;
 
-	public int tailLength = 2;
-	public int direction;
-	public int score = 0;
+	public static int tailLength = 2;
+	public static int direction;
+	public static int score = 0;
+	public static int ticks = 0;
 
 	public boolean over = false;
+	public static boolean needApple = true;
 
-	public Timer timer = new Timer(20, this);
+	public Timer timer = new Timer(60, this);
 
 	public Dimension dim;
 
 	public JFrame frame;
 
 	public Point head;
-	public Point apple;
-
-	private Random random;
+	public static Point apple;
 
 	public static Map m;
 	public static RenderPanel renderPanel;
@@ -58,14 +63,23 @@ public class Main implements ActionListener, KeyListener {
 		frame.setResizable(false);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(renderPanel = new RenderPanel());
+		frame.addKeyListener(this);
 		frame.setVisible(true);
 
 		startGame();
+		readFile();
 	}
 
 	public void startGame() {
-
 		over = false;
+
+		if (tailLength == 5) {
+			level = 2;
+			tailLength = 2;
+			score = 0;
+			direction = DOWN;
+			head = new Point(1, 1);
+		}
 
 		if (level == 1) {
 			tailLength = 2;
@@ -75,6 +89,7 @@ public class Main implements ActionListener, KeyListener {
 		}
 
 		snakeParts.clear();
+		readFile();
 		timer.start();
 	}
 
@@ -82,35 +97,126 @@ public class Main implements ActionListener, KeyListener {
 	public void actionPerformed(ActionEvent e) {
 		renderPanel.repaint();
 
+		if (!over) {
+			ticks++;
+		}
+
+		if (ticks % 2 == 0 && !over) {
+			snakeParts.add(new Point(head.x, head.y));
+
+			if (snakeParts.size() > tailLength) {
+				snakeParts.remove(0);
+			}
+
+			if (direction == UP) {
+				if (!getMap(head.x, head.y - 1).equals("w") && noTailAt(head.x, head.y - 1)) {
+					head = new Point(head.x, head.y - 1);
+				} else {
+					over = true;
+				}
+			}
+
+			if (direction == DOWN) {
+				if (!getMap(head.x, head.y + 1).equals("w") && noTailAt(head.x, head.y + 1)) {
+					head = new Point(head.x, head.y + 1);
+				} else {
+					over = true;
+				}
+			}
+
+			if (direction == LEFT) {
+				if (!getMap(head.x - 1, head.y).equals("w") && noTailAt(head.x - 1, head.y)) {
+					head = new Point(head.x - 1, head.y);
+				} else {
+					over = true;
+				}
+			}
+
+			if (direction == RIGHT) {
+				if (!getMap(head.x + 1, head.y).equals("w") && noTailAt(head.x + 1, head.y)) {
+					head = new Point(head.x + 1, head.y);
+				} else {
+					over = true;
+				}
+			}
+
+			if (apple != null) {
+				if (head.equals(apple)) {
+					score += 10;
+					tailLength++;
+					needApple = true;
+				}
+			}
+
+			if (tailLength == 5 && level == 1) {
+				startGame();
+			}
+		}
+	}
+
+	private boolean noTailAt(int x, int y) {
+		for (Point point : snakeParts) {
+			if (point.equals(new Point(x, y))) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		int i = e.getKeyCode();
 
-		if (i == KeyEvent.VK_W) {
+		if (i == KeyEvent.VK_W && direction != DOWN && !over) {
 			direction = UP;
 		}
-		if (i == KeyEvent.VK_S) {
+		if (i == KeyEvent.VK_S && direction != UP && !over) {
 			direction = DOWN;
 		}
-		if (i == KeyEvent.VK_A) {
+		if (i == KeyEvent.VK_A && direction != RIGHT && !over) {
 			direction = LEFT;
 		}
-		if (i == KeyEvent.VK_D) {
+		if (i == KeyEvent.VK_D && direction != LEFT && !over) {
 			direction = RIGHT;
 		}
+		if (i == KeyEvent.VK_SPACE) {
+			if (over) {
+				startGame();
+			}
+		}
+
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// not used
-
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
 		// not used
+	}
+
+	public void readFile() {
+
+		try {
+			scn = new Scanner(new File("res/level" + level + ".txt"));
+		} catch (Exception e) {
+			System.err.println("Error Loading Map!");
+		}
+
+		while (scn.hasNext()) {
+			for (int i = 0; i < 25; i++) {
+				Map[i] = scn.next();
+			}
+		}
+
+		scn.close();
+	}
+
+	public String getMap(int x, int y) {
+		String index = Map[y].substring(x, x + 1);
+		return index;
 	}
 
 	public static void main(String[] args) {
--- Snake v1.1/src/ua/kas/snake/RenderPanel.java
+++ Snake v1.1/src/ua/kas/snake/RenderPanel.java
@@ -1,15 +1,22 @@
 package ua.kas.snake;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
 public class RenderPanel extends JPanel {
 
+	private static final long serialVersionUID = 1L;
+
 	private Map m;
 
-	private static final long serialVersionUID = 1L;
+	private Random random;
+
+	public ArrayList<String> placeForApple = new ArrayList<String>();
 
 	@Override
 	protected void paintComponent(Graphics g) {
@@ -22,6 +29,7 @@ public class RenderPanel extends JPanel {
 			for (int x = 0; x < 30; x++) {
 				if (m.getMap(x, y).equals("g")) {
 					g.drawImage(m.getGrass(), x * Main.SCALE, y * Main.SCALE, null);
+					placeForApple.add(Integer.toString(x) + ";" + Integer.toString(y));
 				}
 				if (m.getMap(x, y).equals("w")) {
 					g.drawImage(m.getWall(), x * Main.SCALE, y * Main.SCALE, null);
@@ -29,24 +37,44 @@ public class RenderPanel extends JPanel {
 			}
 		}
 
-		// g.drawImage(m.getApple(), main.apple.x * Main.SCALE, main.apple.y *
-		// Main.SCALE, null);
+		if (Main.needApple) {
 
-		for (Point point : main.snakeParts) {
+			random = new Random();
+			String s = placeForApple.get(random.nextInt(placeForApple.size()));
+
+			Main.apple = new Point(Integer.parseInt(s.substring(0, s.indexOf(";"))),
+					Integer.parseInt(s.substring(s.indexOf(";") + 1)));
+			Main.needApple = false;
+
+			placeForApple.clear();
+		}
+
+		g.drawImage(m.getApple(), Main.apple.x * Main.SCALE, Main.apple.y * Main.SCALE, null);
+
+		for (Point point : Main.snakeParts) {
 			g.drawImage(m.getTail(), point.x * Main.SCALE, point.y * Main.SCALE, null);
 		}
 
-		if (main.direction == 0) {
+		if (Main.direction == 0) {
 			g.drawImage(m.getHeadUP(), main.head.x * Main.SCALE, main.head.y * Main.SCALE, null);
 		}
-		if (main.direction == 1) {
+		if (Main.direction == 1) {
 			g.drawImage(m.getHeadDOWN(), main.head.x * Main.SCALE, main.head.y * Main.SCALE, null);
 		}
-		if (main.direction == 2) {
+		if (Main.direction == 2) {
 			g.drawImage(m.getHeadLEFT(), main.head.x * Main.SCALE, main.head.y * Main.SCALE, null);
 		}
-		if (main.direction == 3) {
+		if (Main.direction == 3) {
 			g.drawImage(m.getHeadRIGHT(), main.head.x * Main.SCALE, main.head.y * Main.SCALE, null);
 		}
+
+		if (main.over) {
+			g.setColor(Color.BLACK);
+			g.fillRect(0, 0, 966, 829);
+			g.setColor(Color.WHITE);
+			g.drawString("You`re lose!" + " Score: " + Main.score, 450, 400);
+			g.drawString(" Time: " + Main.ticks / 2 / 20, 480, 420);
+		}
+
 	}
 }
