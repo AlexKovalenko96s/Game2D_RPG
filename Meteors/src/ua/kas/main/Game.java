4be1384d70af41adf66ae4be78bb2638fb6c4a75
Alex Kovalenko <alex.kovalenko.s96@gmail.com>
2016-09-02 20:20:21 +0300

    game loop

---
+++ Meteors/src/ua/kas/main/Game.java
@@ -0,0 +1,95 @@
+package ua.kas.main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
+
+	public static final int WIDTH = 640;
+	public static final int HEIGHT = WIDTH / 12 * 9;
+
+	private boolean running = false;
+
+	private Thread thread;
+
+	public Game() {
+		new Window(WIDTH, HEIGHT, "Meteors", this);
+	}
+
+	public synchronized void start() {
+		if (running) {
+			return;
+		}
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		try {
+			thread.join();
+			running = false;
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		long lastTime = System.nanoTime();
+		double amountOfTicks = 60.0;
+		double ns = 1000000000 / amountOfTicks;
+		double delta = 0;
+
+		double updates = 0;
+		double frames = 0;
+		long timer = System.currentTimeMillis();
+
+		while (running) {
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+			while (delta >= 1) {
+				tick();
+				delta--;
+				updates++;
+			}
+			render();
+			frames++;
+			if (System.currentTimeMillis() - timer > 1000) {
+				System.out.println("Ticks: " + updates + " FPS: " + frames);
+				timer += 1000;
+				updates = 0;
+				frames = 0;
+			}
+		}
+		stop();
+	}
+
+	private void tick() {
+
+	}
+
+	private void render() {
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(2);
+			return;
+		}
+		Graphics g = bs.getDrawGraphics();
+
+		g.setColor(Color.CYAN);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+
+		g.dispose();
+		bs.show();
+	}
+
+	public static void main(String[] args) {
+		new Game();
+	}
+}
---
+++ Meteors/src/ua/kas/main/Window.java
@@ -0,0 +1,29 @@
+package ua.kas.main;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Window extends Canvas {
+
+	private static final long serialVersionUID = 1L;
+
+	public Game game;
+
+	public Window(int width, int height, String title, Game game) {
+		this.game = game;
+		JFrame frame = new JFrame(title);
+
+		frame.setPreferredSize(new Dimension(width, height));
+		frame.setMinimumSize(new Dimension(width, height));
+		frame.setMaximumSize(new Dimension(width, height));
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+		frame.add(game);
+		frame.setVisible(true);
+		game.start();
+	}
+}
